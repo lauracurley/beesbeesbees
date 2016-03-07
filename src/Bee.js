75d30c36eec2628d96e83a4287d25daf90af1047
Laura Curley <info@lauracurley.com>
2016-03-07 12:27:37 -0800

    Complete all prompts for BeesBeesBees pseudoclassical subclasses

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this); //grabs all the properties of Gub
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+};
+
+Grub.prototype.constructor = Grub;
+
+Grub.prototype.eat = function() {
+  console.log(this.color);
 };
 
